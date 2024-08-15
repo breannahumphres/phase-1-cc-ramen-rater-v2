682bcb55c3d8f93297856cc9132d22aea8227584
Bre Humphres <breannahumphres@gmail.com>
2024-08-15 13:03:59 -0700

    partially completed handle click

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 // index.js
 
 // Callbacks
-const handleClick = (ramen) => {
+const handleClick = () => {
   // Add code
-};
+    const ramenMenu = document.querySelector("#ramen-menu");
+      ramenMenu.addEventListener("click", function(event) {
+        const ramenImg = event.target;
+        if (ramenImg.tagName ==="IMG"){
+          const detailImage = document.querySelector("#ramen-detail .detail-image");
+          detailImage.src = ramenImg.src;
+          const nameElement = document.querySelector("#ramen-detail .name");
+          nameElement.innerHTML = ramenImg.dataset.ramenName;
+          const restaurant = document.querySelector("#ramen-detail .restaurant");
+          restaurant.innerHTML = ramenImg.dataset.ramenRestuarant;
+        }
+         
+          });
+ };
+
+
 
 const addSubmitListener = () => {
   // Add code
+ 
+
 }
 
 const displayRamens = () => {
@@ -16,18 +33,23 @@ const displayRamens = () => {
   .then(ramens => 
      { 
       //console.log(ramen)
-  const ramenMenu = document.querySelector("#ramen-menu")
+      const ramenMenu = document.querySelector("#ramen-menu")
+      ramenMenu.innerHTML = "";
   ramens.forEach(ramen => {
-    const ramenImg = document.createElement("img")
+    const ramenImg = document.createElement("img");
     ramenImg.src = ramen.image
+    ramenImg.dataset.ramenName = ramen.name;
+    ramenImg.dataset.ramenRestuarant = ramen.restaurant;
     ramenMenu.append(ramenImg)
   });
+handleClick();
   })
   .catch(error => console.log(error));
 }
 const main = () => {
   // Invoke displayRamens here
   // Invoke addSubmitListener here
+  displayRamens();
 }
 main()
 
